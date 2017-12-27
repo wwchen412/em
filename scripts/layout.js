37ad68b84ad8ba1c5959ee641a35fb51d4f955c9
wwchen <wwchen412@gmail.com>
2017-12-27 19:40:40 +0800

    contact 缺手機

--- scripts/layout.js
+++ scripts/layout.js
@@ -1,5 +1,5 @@
 $(function() {
-  $("#header").load("index.html #header");
-  $("footer").load('index.html footer');
+  $("#header").load("index.html #header nav");
+  $("footer").load('index.html footer > ul');
   // $(".slider-footer").load(".slider-footer footer");
 });
