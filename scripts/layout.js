3f533840bd034a27f06a80872af757dfc38155e1
wwchen <wwchen412@gmail.com>
2017-12-27 19:37:39 +0800

    contact

--- scripts/layout.js
+++ scripts/layout.js
@@ -1,5 +1,5 @@
 $(function() {
   $("#header").load("index.html #header");
   $("footer").load('index.html footer');
-  $(".slider-footer").load(".slider-footer footer");
+  // $(".slider-footer").load(".slider-footer footer");
 });
